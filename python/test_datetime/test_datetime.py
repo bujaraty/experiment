5ed55f660d1d0b9433bcb17d686884f580a36188
jessada <jessada@kth>
2012-11-12 12:44:44 +0100

    add python/test_datetime

---
+++ python/test_datetime/test_datetime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import datetime, time;
+
+
+count = 0    
+while (1):
+    print '************** check last minute round', count, ' at', str(datetime.datetime.now()), '****************'
+    print str(datetime.datetime.now())
+    
+        
+    
+    time.sleep(3)
+    
+    count += 1 
