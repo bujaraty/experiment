42e3cb3efa0b47fc365d90746f44aa2164ace4ee
jessada <jessada@kth>
2012-12-03 17:58:30 +0100

    learn how to package python and unit test

---
+++ python/test_packaging/MyTowel/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+	name='MyTowel',
+	version='0.1.0',
+	author='test A. bc',
+	author_email='tester@test.com',
+	packages=['mytowel', 'mytowel.test'],
+	scripts=['bin/test_bin.py'],
+	url='http://pypi.python.org/pypi/TowelStuff/',
+	license='LICENSE.txt',
+	description='Useful MyTowel stuff.',
+	long_description=open('README.txt').read(),
+	install_requires=[
+	    "pyVCF >= 0.6.1",
+	    "nose >= 1.2.1",
+	    ],
+	)
