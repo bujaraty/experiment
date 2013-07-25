77854d32c253d99e85cc0276059c7f2507eca417
jessada <jessada@kth>
2013-07-25 12:40:56 +0200

    done with testing excel read/write

---
+++ python/test_xlrd/test_xlrd_normal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# tries stress SST, SAT and MSAT
+
+#import xlrd
+#from xlrd.timemachine import REPR
+import xlrd
+workbook = xlrd.open_workbook('namesdemo.xls')
+worksheet = workbook.sheet_by_name('Sheet3')
+num_rows = worksheet.nrows - 1
+num_cells = worksheet.ncols - 1
+curr_row = -1
+while curr_row < num_rows:
+    curr_row += 1
+    row = worksheet.row(curr_row)
+    print 'Row:', curr_row
+    curr_cell = -1
+    while curr_cell < num_cells:
+        curr_cell += 1
+        #print Cell Types: 0=Empty, 1=Text, 2=Number, 3=Date, 4=Boolean, 5=Error, 6=Blank
+        cell_type = worksheet.cell_type(curr_row, curr_cell)
+        cell_value = worksheet.cell_value(curr_row, curr_cell)
+        print '', cell_type, ':', cell_value
